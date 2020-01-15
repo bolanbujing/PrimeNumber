96fdc33e7e1f24fb99fdffe899af7b42c89f22b4
unknown <liuxiaobo@bianfeng.com>
2020-01-15 14:16:01 +0800

    fix

---
+++ test.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func Processor(seq chan int, wait chan struct{}) {
+	go func() {
+		prime, ok := <-seq
+		if !ok {
+			close(seq)
+			return
+		}
+		fmt.Println(prime)
+		out := make(chan int)
+		Processor(out, wait)
+		for num := range seq {
+			if num%prime != 0 {
+				out <- num
+			}
+		}
+		close(out)
+	}()
+
+}
+func main() {
+	orgin, wait := make(chan int), make(chan struct{})
+
+	Processor(orgin, wait)
+	for num := 2; num < 10000; num++ {
+		orgin <- num
+	}
+	close(orgin)
+	<-wait
+}
